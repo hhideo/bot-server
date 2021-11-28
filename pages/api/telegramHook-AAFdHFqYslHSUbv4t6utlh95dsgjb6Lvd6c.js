9ba55bf014494b45d6388a535766fd425513ce01
hhideo <h_hideo_83@hotmail.com>
2021-11-27 23:39:17 -0300

    Telegram bot echoing messages

--- pages/api/telegramHook-AAFdHFqYslHSUbv4t6utlh95dsgjb6Lvd6c.js
+++ pages/api/telegramHook-AAFdHFqYslHSUbv4t6utlh95dsgjb6Lvd6c.js
@@ -1,12 +1,12 @@
 async function telegramHook(request, response) {
     const telegramUrl = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}`
 
-    console.log(request.body);
+    const { message } = request.body;
 
     const reqOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ chat_id: 55677412, text: 'Requisição recebida' })
+        body: JSON.stringify({ chat_id: message.from.id, text: `Echo: ${message.text}` })
     }
     const sentMessage = await fetch(telegramUrl + '/sendMessage', reqOptions);
 
