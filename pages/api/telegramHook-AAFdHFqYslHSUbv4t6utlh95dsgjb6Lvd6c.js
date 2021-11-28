e21184a21bf4eb7835aed2e0860382bd0f37ebe2
hhideo <h_hideo_83@hotmail.com>
2021-11-27 23:34:28 -0300

    Second Telegram hook test

--- pages/api/telegramHook-AAFdHFqYslHSUbv4t6utlh95dsgjb6Lvd6c.js
+++ pages/api/telegramHook-AAFdHFqYslHSUbv4t6utlh95dsgjb6Lvd6c.js
@@ -1,14 +1,16 @@
 async function telegramHook(request, response) {
     const telegramUrl = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}`
 
+    console.log(request.body);
+
     const reqOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ chat_id: 55677412, text: 'Requisição recebida' })
     }
-    fetch(telegramUrl + '/sendMessage', reqOptions);
+    const sentMessage = await fetch(telegramUrl + '/sendMessage', reqOptions);
 
-    response.send();
+    response.json(await sentMessage.json());
 }
 
 export default telegramHook;
